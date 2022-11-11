c980b7f88695fb6267afa6cf5fb319bb69c3619d
icdowns <idowns@hawk.iit.edu>
2022-11-11 11:25:41 -0600

    fixed up some js

--- javascript/script.js
+++ javascript/script.js
@@ -1,11 +1,13 @@
 function init(){
 //add your javascrip between these two lines of code
   function AlertMe(){
-	  var input = document.getElementById('entryinput');
-	  var clickButton = document.getElementById('entrybutton');
-	  var output = document.getElementById('textoutput');
-	  var names = document.getElementByName('Immanuel Downs');
-	  alert(names.value + ": " + input.value)
+	  var input = document.getElementById('entryinput')
+	  var clickButton = document.getElementById('entrybutton')
+	  var output = document.getElementById('textoutput')
+	 
+	  alert('Immanuel Downs:' + input.value);
+	  output.innerHTML = input.value;
+	  
   }
   
   pressButton.addEventListener('click', Alertme);
